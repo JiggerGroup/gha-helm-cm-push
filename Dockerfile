61a682dd8a4ae578a8e80d8e180d06ddc7e51875
ArlonAntonius <37894143+ArlonAntonius@users.noreply.github.com>
2022-02-15 09:32:38 +0100

    Init

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM plugins/base:linux-amd64
+
+ENV XDG_DATA_HOME=/opt/xdg
+ENV XDG_CACHE_HOME=/opt/xdg
+
+RUN apk add curl tar bash --no-cache
+
+RUN set -ex \
+    && curl -sSL https://get.helm.sh/helm-v3.8.0-linux-amd64.tar.gz | tar xz \
+    && mv linux-amd64/helm /usr/local/bin/helm \
+    && rm -rf linux-amd64
+
+RUN helm plugin install https://github.com/chartmuseum/helm-push
+
+COPY entrypoint.sh /entrypoint.sh
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+if [ -z "${CHART_FOLDER}" ]; then
+  echo "Chart folder is required but not defined."
+  exit 1
+fi
+
+if [ -z "${REGISTRY_URL}" ]; then
+  echo "Repository url is required but not defined."
+  exit 1
+fi
+
+if [ -z "${REGISTRY_USERNAME}" ] || [ -z "${REGISTRY_PASSWORD}" ]; then
+  echo "Credentials are required, but none defined."
+  exit 1
+fi
+
+if [ "${FORCE}" == "1" ] || [ "${FORCE}" == "True" ] || [ "${FORCE}" == "TRUE" ] || [ "${FORCE}" == "true" ]; then
+  FORCE="-f"
+else
+  FORCE=""
+fi
+
+if [ "${REGISTRY_USERNAME}" ]; then
+  echo "Username is defined, using as parameter."
+  REGISTRY_USERNAME="--username ${REGISTRY_USERNAME}"
+fi
+
+if [ "${REGISTRY_PASSWORD}" ]; then
+  echo "Password is defined, using as parameter."
+  REGISTRY_PASSWORD="--password ${REGISTRY_PASSWORD}"
+fi
+
+if [ "${REGISTRY_VERSION}" ]; then
+  echo "Version is defined, using as parameter."
+  REGISTRY_VERSION="--version ${REGISTRY_VERSION}"
+fi
+
+if [ "${REGISTRY_APPVERSION}" ]; then
+  echo "App version is defined, using as parameter."
+  REGISTRY_APPVERSION="--app-version ${REGISTRY_APPVERSION}"
+fi
+
+cd ${CHART_FOLDER}
+helm lint .
+helm package . ${REGISTRY_APPVERSION} ${REGISTRY_VERSION}
+helm inspect chart $(find *.tgz)
+helm cm-push $(find *.tgz) ${REGISTRY_ADDRESS} ${REGISTRY_USERNAME} ${REGISTRY_PASSWORD}
